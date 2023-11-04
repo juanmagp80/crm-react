dd8b73771269c30b5d1cb1dd562fa6ab1bdac78c
juanma <juanmagp26@gmail.com>
2023-11-04 14:49:44 +0100

    En este punto vamos a añadir el formulario

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
-const Index = () => {
-    return (
-        <div>
-            <h1>Clientes</h1>
-        </div>
-    )
+export function loader() {
+  const clientes = [
+    {
+      id: 1,
+      nombre: "Juan",
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 2,
+      nombre: "Karen",
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 3,
+      nombre: "Josue",
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 4,
+      nombre: "Miguel",
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+    {
+      id: 5,
+      nombre: "Pedro",
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: "Codigo Con Juan",
+    },
+  ];
+  return clientes;
 }
 
-export default Index
+const Index = () => {
+  const clientes = useLoaderData();
+  console.log(clientes);
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3 text-gray-700 text-base"> Administra tus clientes</p>
+
+      {clientes.length ? (
+        <table className="table-auto bg-white shadow-md mt-10 w-full w-lg">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Nombre</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {clientes.map((cliente) => (
+              <Cliente cliente={cliente} key={cliente.id} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="mt-5 text-center text-gray-600">
+          No hay clientes, comienza creando uno
+        </p>
+      )}
+    </>
+  );
+};
+
+export default Index;
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const NuevoCliente = () => {
-    return (
-        <div>
-            <h1 className='text-2xl text-gray-800 font-light'>Nuevo Cliente</h1>
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default NuevoCliente
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3 text-gray-700 text-base">
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+      <div className="flex justify-end mt-5">
+        <button
+          className="bg-blue-800 py-2 px-4 text-white font-bold uppercase rounded text-sm hover:bg-blue-900"
+          onClick={() => navigate("/")}
+        >
+          Volver
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 px-5 py-10 ">
+        <p className="font-bold text-gray-800 text-xl mb-5">Formulario aqui</p>
+      </div>
+    </>
+  );
+};
+
+export default NuevoCliente;
